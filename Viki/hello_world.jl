c072a8ae1d95e8cb95763f99002deb9eaa6fda85
Haghani <vhaghani@ucdavis.edu>
2021-06-29 12:31:28 -0700

    add subdirectory for Viki

---
+++ Viki/hello_world.jl
@@ -0,0 +1,6 @@
+# To run the code, use the command: include(filename)
+# Do this in the julia prompt
+# Example: include("hello_world.jl")
+
+example_string = "Hello, world"
+println(example_string)
