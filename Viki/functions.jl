103d12b25598a8e6cb0c27982e8d33210563308f
Haghani <vhaghani@ucdavis.edu>
2021-07-01 10:20:46 -0700

    initial upload

---
+++ Viki/functions.jl
@@ -0,0 +1,19 @@
+# Functions are typically designated by the function name and parentheses, with an argument in the parentheses 
+println("println is a function and this is the argument")
+
+# If there is no input, a blank line is printed
+println()
+
+# Operators are exceptions to the parentheses rule
+a = 12 + 14
+println(a)
+
+# Creating a basic function
+function add_numbers(x, y)
+    x + y
+    end
+
+# Using the function
+println(add_numbers(1, 2))
+println(add_numbers(2, 3))
+println(add_numbers(3, 4))
