3afa81cacbad23a9ab69b8fa4f59a63f5b8266ef
Haghani <vhaghani@ucdavis.edu>
2021-07-01 10:20:14 -0700

    initial upload

---
+++ Viki/characters_vs_strings.jl
@@ -0,0 +1,14 @@
+# Using single quotes results in a character
+a = 'b'
+println(a)
+
+# Using double quotes results in a string
+a = "b"
+println(a)
+
+
+# On the command line, using the command a = 'b' results in the output
+# 'b': ASCII/Unicode U+0062 (category Ll: Letter, lowercase)
+
+# Using the command a = "b" results in the output
+# "b"
