e32beb331610b5d6810bf77147a69b7191fa9092
meghana202 <meghymail@gmail.com>
2021-07-02 14:20:50 -0700

    added orf.jl

---
+++ Meghana/orf.jl
@@ -0,0 +1,26 @@
+using BioSequences
+function orf()
+    jay = 0
+    genome = randdnaseq(500)
+    orfs = Dict()
+    for i = 1:498
+        codon = translate(genome[i:i+2])
+        if codon == aa"M"
+            for j = i:3:498
+                codon = translate(genome[j:j+2])
+                if codon == aa"*"
+                    jay = j
+                    break
+                end 
+            end 
+            length_ = jay - i 
+            if haskey(orfs, length_)
+                orfs[length_]+= 1
+            else 
+                orfs[length_] =1 
+            end
+        end 
+    end 
+    println(orfs)
+end 
+orf() 
